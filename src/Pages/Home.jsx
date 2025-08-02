f6c2be68afe494207daeada8070a5f08abe3ce13
AssaultMachete <assualtmachete20@gmail.com>
2025-08-03 01:03:43 +0300

    HOOMECOOMIN

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,12 @@
 const Home = () => {
   return (
-    <h1 className="text-white text-4xl text-center">
-      This a Gallery I tried to create for you, and also practice a
-      little..enjoy it
-    </h1>
+    <div className="flex flex-col items-center justify-center gap-5">
+      <h1 className="text-white text-4xl text-center">
+        This a Gallery I tried to create for you, and also practice a
+        little..enjoy it
+      </h1>
+      <img src="public/1299033846372634725.png" className="w-100" />
+    </div>
   );
 };
 export default Home;
